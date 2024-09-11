a10a00105740e30d0b6f2f2729375d029e84135f
jason <jason36375667@gmail.com>
2024-09-11 18:28:54 +0800

    adding math area prefab

--- Assets/BallTextDisplay.cs
+++ Assets/BallTextDisplay.cs
@@ -5,7 +5,7 @@ public class BallTextDisplay : MonoBehaviour
 {
     public Rigidbody2D ballRigidbody;    // The Rigidbody2D component of the ball
     public TextMeshProUGUI numText;     // Reference to the TextMeshPro component on the ball
-
+    public int num = 1;                 // The number to display
     void Update()
     {
         // Get the current velocity of the ball
@@ -13,9 +13,21 @@ public class BallTextDisplay : MonoBehaviour
 
         // Calculate the magnitude of the velocity (speed)
         // float speed = velocity.magnitude;
-        int num = 1;
 
         // Update the TextMeshPro text to display the speed
         numText.text = num.ToString();  // "F2" limits to 2 decimal places
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("MathArea"))  // Make sure to tag the wall areas as "MathArea"
+        {
+            MathOperationDisplay mathOp = collision.gameObject.GetComponent<MathOperationDisplay>();
+            if (mathOp != null)
+            {
+                int currentValue = int.Parse(numText.text);
+                num = mathOp.ApplyOperation(currentValue);
+            }
+        }
     }
 }
---
+++ Assets/MathOpDisplay.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using TMPro;
+
+public class MathOperationDisplay : MonoBehaviour
+{
+    public string mathOperation = "+";   // The operation (+, -, ×, ÷)
+    public int number = 1;               // The number for the operation
+    public TextMeshProUGUI operationText;    // Reference to the TextMeshPro component
+
+    void Start()
+    {
+        // Set the initial text to display the operation and number
+        UpdateOperationDisplay();
+    }
+
+    // Function to update the displayed text
+    public void UpdateOperationDisplay()
+    {
+        operationText.text = mathOperation + " " + number.ToString();
+    }
+
+    // Function to dynamically change the operation and number
+    public void SetOperation(string newOperation, int newNumber)
+    {
+        mathOperation = newOperation;
+        number = newNumber;
+        UpdateOperationDisplay();
+    }
+
+    // You can call this function when the ball interacts with the area to apply the effect
+    public int ApplyOperation(int ballValue)
+    {
+        switch (mathOperation)
+        {
+            case "+":
+                return ballValue + number;
+            case "-":
+                return ballValue - number;
+            case "×":
+                return ballValue * number;
+            case "÷":
+                return ballValue / number;
+            default:
+                return ballValue;
+        }
+    }
+}
